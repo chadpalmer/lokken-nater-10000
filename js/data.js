6aaa89e8e42bf7891935465f3e23ec0fba303744
Chad Palmer <chadpalmer@gmail.com>
2015-06-28 18:54:57 -0400

    Added rest of files and got app up and running.

--- js/data.js
+++ js/data.js
@@ -1,51 +1,57 @@
 var data = {
     "family": [
         {
-            name: "Mike",
+            "name": "Mike",
             "quote": "I weep for the liberty of my country when I see at this early day of its successful experiment that corruption has been imputed to many members of the House of Representatives, and the rights of the people have been bartered for promises of office.",
             "photo": "mike.jpg",
             "credit": "Andrew Jackson"
         },
         {
-            name: "Jen",
+            "name": "Jen",
             "quote": "When the people fear the government, there is tyranny. When the government fears the people, there is liberty.",
             "photo": "jen.jpg",
             "credit": "Thomas Jefferson"
         },
         {
-            name: "Selah",
+            "name": "Selah",
             "quote": "I sincerely believe... that banking establishments are more dangerous than standing armies.",
             "photo": "selah.jpg",
             "credit": "Thomas Jefferson"
         },
         {
-            name: "Mitch",
+            "name": "Mitch",
             "quote": "They who can give up essential liberty to obtain a little temporary safety deserve neither liberty nor safety.",
             "photo": "mitch.jpg",
             "credit": "Benjamin Franklin"
         },
         {
-            name: "Josh",
+            "name": "Josh",
             "quote": "Better to die fighting for freedom then be a prisoner all the days of your life.",
             "photo": "josh.jpg",
             "credit": "Bob Marley"
         },
         {
-            name: "Natalie",
+            "name": "Natalie",
             "quote": "If liberty means anything at all, it means the right to tell people what they do not want to hear.",
             "photo": "natalie.jpg",
             "credit": "George Orwell"
         },
         {
-            name: "Brianne",
+            "name": "Brianne",
             "quote": "I would rather be exposed to the inconveniences attending too much liberty than to those attending too small a degree of it.",
-            "photo": "brianne",
+            "photo": "brianne.jpg",
             "credit": "Thomas Jefferson"
         },
         {
-            name: "Bella",
+            "name": "Bella",
             "quote": "People have only as much liberty as they have the intelligence to want and the courage to take.",
             "photo": "bella.jpg",
             "credit": "Emma Goldman"
         }
-    ]};
+    ],
+    "cool_friend": {
+        "name": "Chad",
+        "quote": "Since you left the law text box blank, I see you don't want any more laws. I like your style!",
+        "photo": "chad.jpg",
+        "credit": "Chad Palmer"
+    }};
--- js/js.js
+++ js/js.js
@@ -1,9 +1,89 @@
-objLokken = objLokken || {}
+var objLokken = objLokken || {};
 
-objLokken.init = function() {
+objLokken.currentQuote = 0;
+
+objLokken.init = function () {
+    objLokken.data = data;
+    objLokken.shuffleAnswers(objLokken.data.family);
+    objLokken.audio = document.getElementById("quote");
+    objLokken.mp3 = document.getElementById("mp3");
+    objLokken.ogg = document.getElementById("ogg");
+    objLokken.photo = document.getElementById("photo");
+    objLokken.text = document.getElementById("quote_text");
+    objLokken.quoteBox = document.getElementById("quote_box");
+    objLokken.lawBox = document.getElementById("law_entry");
+
+    objLokken.audio.addEventListener("ended", function () {
+        setTimeout(function () {
+            objLokken.fade(false, 1)
+        }, 100);
+    });
+};
+
+objLokken.renderQuote = function () {
+    if (objLokken.lawBox.value != "") {
+        objLokken.photo.src = "images/" + objLokken.data.family[objLokken.currentQuote].photo;
+        objLokken.mp3.src = "audio/" + objLokken.data.family[objLokken.currentQuote].name.toLowerCase() + ".mp3";
+        objLokken.ogg.src = "audio/" + objLokken.data.family[objLokken.currentQuote].name.toLowerCase() + ".ogg";
+        objLokken.audio.load();
+        objLokken.text.innerHTML = objLokken.data.family[objLokken.currentQuote].quote + "<br/><span id=\"credit\">" + objLokken.data.family[objLokken.currentQuote].credit + "</span>";
+        setTimeout(function () {
+            objLokken.fade(true, 0)
+        }, 100);
+        objLokken.currentQuote = objLokken.currentQuote + 1;
+    } else {
+        objLokken.photo.src = "images/" + objLokken.data.cool_friend.photo;
+        objLokken.mp3.src = "audio/" + objLokken.data.cool_friend.name.toLowerCase() + ".mp3";
+        objLokken.ogg.src = "audio/" + objLokken.data.cool_friend.name.toLowerCase() + ".ogg";
+        objLokken.audio.load();
+        objLokken.text.innerHTML = objLokken.data.cool_friend.quote + "<br/><span id=\"credit\">" + objLokken.data.cool_friend.credit + "</span>";
+        setTimeout(function () {
+            objLokken.fade(true, 0)
+        }, 100);
+        objLokken.currentQuote = objLokken.currentQuote + 1;
+    }
 
 };
 
-objLokken.renderQuote = function() {
+objLokken.fade = function (blnFadeIn, numStart) {
+    if (blnFadeIn) {
+        numStart = numStart + .2;
+        objLokken.quoteBox.style.opacity = numStart;
+        if (numStart < 1) {
+            setTimeout(function () {
+                objLokken.fade(true, numStart)
+            }, 100);
+        } else {
+            setTimeout(function () {
+                objLokken.audio.play()
+            }, 400);
+        }
+    } else {
+        numStart = numStart - .2;
+        objLokken.quoteBox.style.opacity = numStart;
+        if (numStart > 0) {
+            setTimeout(function () {
+                objLokken.fade(false, numStart)
+            }, 100);
+        }
+    }
+};
 
+objLokken.shuffleAnswers = function (array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 };
